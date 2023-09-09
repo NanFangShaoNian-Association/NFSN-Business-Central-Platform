65c841f5ff80c0af210a7443835d484b997b5ac3
AtnibamAitay <atnibamaitay@foxmail.com>
2023-09-09 12:50:38 +0800

    refactor: 把Redis的常量放到了constant类中

--- bcp-common/common-redis/src/main/java/cn/nfsn/common/redis/constant/CacheConstants.java
+++ bcp-common/common-redis/src/main/java/cn/nfsn/common/redis/constant/CacheConstants.java
@@ -6,6 +6,33 @@ package cn.nfsn.common.redis.constant;
  * @date 2023/7/10 20:54
  */
 public class CacheConstants {
+    /**
+     * 冒号字符，常用于键的构造
+     */
     public static final String MQ_IDEMPOTENCY = "MQIdempotency:";
+
+    /**
+     * 冒号字符，常用于键的构造
+     */
     public static final String REQ_IDEMPOTENCY = "ReqIdempotency:";
+
+    /**
+     * 默认等待时长 1 毫秒
+     */
+    public static final long DEF_WAIT_TIME = 1L;
+
+    /**
+     * 默认过期时长 10 秒
+     */
+    public static final long DEF_EXPIRE_TIME = 1000 * 10L;
+
+    /**
+     * 默认重试次数
+     */
+    public static final Integer NO_TRY_COUNT = 0;
+
+    /**
+     * 默认重试休眠时长
+     */
+    public static final Long NO_TRY_SLEEP_TIME = 0L;
 }
