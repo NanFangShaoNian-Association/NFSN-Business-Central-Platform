3a02bd247c6ebff36c829d590492e7045d9211e7
AtnibamAitay <atnibamaitay@foxmail.com>
2023-09-13 16:39:10 +0800

    feat: 新增订单相关服务实现类
    实现了OrderInfoService接口中的创建订单和存储订单二维码方法。在创建订单方法中，加入了分布式锁保证并发安全性，并进行了异常处理。在存储订单二维码方法中，仅更新了订单的二维码字段。

--- bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/service/OrderInfoService.java
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/service/OrderInfoService.java
@@ -1,13 +1,30 @@
 package cn.nfsn.transaction.service;
 
+import cn.nfsn.transaction.model.dto.ProductDTO;
 import cn.nfsn.transaction.model.entity.OrderInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author Atnibam Aitay
-* @description 针对表【order_info】的数据库操作Service
-* @createDate 2023-09-07 14:15:09
-*/
+ * @ClassName: OrderInfoService
+ * @Description: 订单信息服务接口，定义了创建订单和存储订单二维码等方法
+ * @Author: atnibamaitay
+ * @CreateTime: 2023-09-07 14:15
+ **/
 public interface OrderInfoService extends IService<OrderInfo> {
+    /**
+     * 根据商品id创建订单
+     *
+     * @param productDTO 商品信息
+     * @param paymentType 支付类型
+     * @return 返回新建的或者已存在但未支付的订单
+     */
+    OrderInfo createOrderByProductId(ProductDTO productDTO, String paymentType) throws InterruptedException;
 
+    /**
+     * 存储订单二维码
+     *
+     * @param orderNo 订单号
+     * @param codeUrl 二维码地址
+     */
+    void saveCodeUrl(String orderNo, String codeUrl);
 }
