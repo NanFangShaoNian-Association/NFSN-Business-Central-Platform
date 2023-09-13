998b7467618e89e6738fa6ba2719c81b80a1ae4a
AtnibamAitay <atnibamaitay@foxmail.com>
2023-09-13 16:23:15 +0800

    feat: 新增WxPayConstant类并添加微信支付密钥
    此次提交导入了微信支付的apiclient_key.pem秘钥文件，并在新的WxPayConstant类中定义了该秘钥的路径常量。

---
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/constant/WxPayConstant.java
@@ -0,0 +1,15 @@
+package cn.nfsn.transaction.constant;
+
+/**
+ * @ClassName: WxPayConstant
+ * @Description: 微信支付常量
+ * @Author: atnibamaitay
+ * @CreateTime: 2023/9/8 0008 16:53
+ **/
+public class WxPayConstant {
+
+    /**
+     * 秘钥路径
+     */
+    public static final String PRIVATE_KEY_PATH = "bcp-modules/modules-transaction/src/main/resources/apiclient_key.pem";
+}
