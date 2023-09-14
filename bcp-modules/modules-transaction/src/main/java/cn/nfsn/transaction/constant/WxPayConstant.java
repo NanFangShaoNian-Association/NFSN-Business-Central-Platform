9b4f93d8435742741b96af6b74a35d0d67ee4226
AtnibamAitay <atnibamaitay@foxmail.com>
2023-09-14 16:10:29 +0800

    modify: 微信支付常量类新增错误码和成功码
    在`WxPayConstant`类中，新增了错误码（ERROR_CODE）、成功码（SUCCESS_CODE）、失败信息（ERROR_MSG）、成功信息（SUCCESS_MSG）以及通知验签失败信息（ERROR_VALIDATION_FAILED_MSG），用于微信支付的状态表示和消息传递。

--- bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/constant/WxPayConstant.java
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/constant/WxPayConstant.java
@@ -12,4 +12,29 @@ public class WxPayConstant {
      * 秘钥路径
      */
     public static final String PRIVATE_KEY_PATH = "bcp-modules/modules-transaction/src/main/resources/apiclient_key.pem";
+
+    /**
+     * 错误码
+     */
+    public static final String ERROR_CODE = "ERROR";
+
+    /**
+     * 成功码
+     */
+    public static final String SUCCESS_CODE = "SUCCESS";
+
+    /**
+     * 失败信息
+     */
+    public static final String ERROR_MSG = "失败";
+
+    /**
+     * 成功信息
+     */
+    public static final String SUCCESS_MSG = "成功";
+
+    /**
+     * 通知验签失败信息
+     */
+    public static final String ERROR_VALIDATION_FAILED_MSG = "通知验签失败";
 }
