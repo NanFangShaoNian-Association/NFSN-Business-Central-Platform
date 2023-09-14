e31af6bcd4a2df42d21ac8e0124c520fa6df1c1d
AtnibamAitay <atnibamaitay@foxmail.com>
2023-09-14 13:34:21 +0800

    refactor: 重构微信支付策略的代码，优化了创建订单和发送请求的逻辑，提高了代码的可读性和可维护性。

--- bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/bridge/WxPayNative.java
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/bridge/WxPayNative.java
@@ -4,7 +4,9 @@ import cn.nfsn.transaction.config.WxPayConfig;
 import cn.nfsn.transaction.enums.PayType;
 import cn.nfsn.transaction.enums.WxApiType;
 import cn.nfsn.transaction.enums.WxNotifyType;
+import cn.nfsn.transaction.model.dto.AmountDTO;
 import cn.nfsn.transaction.model.dto.ProductDTO;
+import cn.nfsn.transaction.model.dto.RequestWxCodeDTO;
 import cn.nfsn.transaction.model.entity.OrderInfo;
 import cn.nfsn.transaction.service.OrderInfoService;
 import com.google.gson.Gson;
@@ -12,11 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-import org.apache.http.impl.client.CloseableHttpClient;
 
 import javax.annotation.Resource;
 import java.io.IOException;
@@ -55,7 +57,6 @@ public class WxPayNative implements IPayMode {
     public Map<String, Object> createOrder(ProductDTO productDTO) throws Exception {
         // 打印日志，开始生成订单
         log.info("开始生成订单");
-        System.out.println(productDTO);
 
         //根据商品ID和支付类型创建订单
         OrderInfo orderInfo = orderInfoService.createOrderByProductId(productDTO, PayType.WXPAY.getType());
@@ -64,99 +65,120 @@ public class WxPayNative implements IPayMode {
         String codeUrl = orderInfo.getCodeUrl();
 
         //检查订单是否存在且二维码URL是否已保存
-        if(orderInfo != null && !StringUtils.isEmpty(codeUrl)){
+        if (orderInfo != null && !StringUtils.isEmpty(codeUrl)) {
             // 添加订单号到日志
             log.info("订单：{} 已存在，二维码已保存", orderInfo.getOrderNo());
 
-            //初始化返回结果映射
-            Map<String, Object> map = new HashMap<>();
-            map.put("codeUrl", codeUrl);
-            map.put("orderNo", orderInfo.getOrderNo());
-
             //返回二维码和订单号
-            return map;
+            return createResultMap(codeUrl, orderInfo.getOrderNo());
         }
 
         //打印日志，开始调用统一下单API
         log.info("订单：{} 开始调用统一下单API", orderInfo.getOrderNo());
 
-        //创建POST请求
-        HttpPost httpPost = new HttpPost(wxPayConfig.getDomain().concat(WxApiType.NATIVE_PAY.getType()));
-
-        //设置请求参数
-        Map paramsMap = new HashMap();
-        paramsMap.put("appid", wxPayConfig.getAppid());
-        paramsMap.put("mchid", wxPayConfig.getMchId());
-        paramsMap.put("description", orderInfo.getTitle());
-        paramsMap.put("out_trade_no", orderInfo.getOrderNo());
-        //设置支付通知的API接口
-        paramsMap.put("notify_url", wxPayConfig.getNotifyDomain().concat(WxNotifyType.NATIVE_NOTIFY.getType()));
+        //获取统一下单API的URL
+        String url = wxPayConfig.getDomain().concat(WxApiType.NATIVE_PAY.getType());
 
         //设置金额参数
-        Map amountMap = new HashMap();
-        amountMap.put("total", orderInfo.getTotalFee());
-        amountMap.put("currency", "CNY");
-        paramsMap.put("amount", amountMap);
+        AmountDTO amountDTO = AmountDTO.builder()
+                .total(orderInfo.getTotalFee())
+                .currency("CNY")
+                .build();
 
-        //将请求参数转换为JSON格式
-        String jsonParams = gson.toJson(paramsMap);
+        //设置请求参数
+        RequestWxCodeDTO requestWxCodeDTO = RequestWxCodeDTO.builder()
+                .appid(wxPayConfig.getAppid())
+                .mchid(wxPayConfig.getMchId())
+                .description(orderInfo.getTitle())
+                .out_trade_no(orderInfo.getOrderNo())
+                .notify_url(wxPayConfig.getNotifyDomain().concat(WxNotifyType.NATIVE_NOTIFY.getType()))
+                .amount(amountDTO)
+                .build();
 
-        //打印请求参数信息
-        log.info("订单：{} 请求参数 ===> {}", orderInfo.getOrderNo(), jsonParams);
+        Map<String, String> resultMap = sendRequest(url, requestWxCodeDTO, "Native下单");
 
-        //设置请求实体
-        StringEntity entity = new StringEntity(jsonParams,"utf-8");
+        //从结果中获取二维码URL
+        codeUrl = resultMap.get("code_url");
+
+        //保存二维码URL
+        String orderNo = orderInfo.getOrderNo();
+        orderInfoService.saveCodeUrl(orderNo, codeUrl);
+
+        //返回二维码和订单号
+        return createResultMap(codeUrl, orderInfo.getOrderNo());
+    }
+
+    /**
+     * 创建结果Map
+     *
+     * @param codeUrl 订单二维码链接
+     * @param orderNo 订单号
+     * @return 包含订单二维码链接和订单号的Map
+     */
+    private Map<String, Object> createResultMap(String codeUrl, String orderNo) {
+        // 创建一个新的HashMap
+        Map<String, Object> map = new HashMap<>();
+
+        // 向map中插入订单二维码链接
+        map.put("codeUrl", codeUrl);
+
+        // 向map中插入订单号
+        map.put("orderNo", orderNo);
+
+        // 返回填充了数据的map
+        return map;
+    }
+
+    /**
+     * 发送请求并获取响应结果
+     *
+     * @param url         请求的URL
+     * @param requestData 请求参数集合
+     * @param logInfo     日志信息
+     * @return 响应结果映射
+     * @throws IOException 如果请求失败，抛出异常
+     */
+    private Map<String, String> sendRequest(String url, Object requestData, String logInfo) throws IOException {
+
+        // 创建POST请求
+        HttpPost httpPost = new HttpPost(url);
+
+        // 将请求参数转换为JSON格式
+        String jsonParams = gson.toJson(requestData);
+
+        // 打印请求参数信息
+        log.info("请求参数 ===> {}", jsonParams);
+
+        // 创建字符串实体，设置编码格式和内容类型
+        StringEntity entity = new StringEntity(jsonParams, "utf-8");
         entity.setContentType("application/json");
+
+        // 设置请求实体和头部信息
         httpPost.setEntity(entity);
         httpPost.setHeader("Accept", "application/json");
 
-        CloseableHttpResponse response = null;
-        try {
-            //发送请求并获取响应
-            response = wxPayClient.execute(httpPost);
-
-            //获取响应内容和状态码
-            //响应体
+        // 发送请求并获取响应
+        try (CloseableHttpResponse response = wxPayClient.execute(httpPost)) {
+            // 获取响应内容和状态码
+            // 响应体
             String bodyAsString = EntityUtils.toString(response.getEntity());
-            //响应状态码
+            // 响应状态码
             int statusCode = response.getStatusLine().getStatusCode();
 
-            //根据状态码处理响应结果
+            // 根据状态码处理响应结果
             if (statusCode == 200) {
-                // 添加订单号到日志
-                log.info("订单：{} 成功, 返回结果 = {}", orderInfo.getOrderNo(), bodyAsString);
+                log.info("成功, 返回结果 = {}", bodyAsString);
             } else if (statusCode == 204) {
-                // 添加订单号到日志
-                log.info("订单：{} 成功", orderInfo.getOrderNo());
+                log.info("成功");
             } else {
-                // 添加订单号到日志
-                log.error("订单：{} Native下单失败,响应码 = {},返回结果 = {}", orderInfo.getOrderNo(), statusCode, bodyAsString);
+                log.info("{}失败,响应码 = {},返回结果 = {}", logInfo, statusCode, bodyAsString);
                 throw new IOException("request failed");
             }
 
-            //解析响应结果
+            // 解析响应结果并返回
             Map<String, String> resultMap = gson.fromJson(bodyAsString, HashMap.class);
 
-            //从结果中获取二维码URL
-            codeUrl = resultMap.get("code_url");
-
-            //保存二维码URL
-            String orderNo = orderInfo.getOrderNo();
-            orderInfoService.saveCodeUrl(orderNo, codeUrl);
-
-            //初始化返回结果映射
-            Map<String, Object> map = new HashMap<>();
-            map.put("codeUrl", codeUrl);
-            map.put("orderNo", orderInfo.getOrderNo());
-
-            //返回二维码和订单号
-            return map;
-
-        } finally {
-            //关闭响应，先检查response是否为null
-            if(response != null){
-                response.close();
-            }
+            return resultMap;
         }
     }
 
