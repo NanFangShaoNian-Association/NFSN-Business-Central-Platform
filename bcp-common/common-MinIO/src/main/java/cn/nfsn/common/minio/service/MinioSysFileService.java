f6550ea3bd33e5b7e62ab68152eaa86fa71eab2d
AtnibamAitay <atnibamaitay@foxmail.com>
2023-09-20 12:24:54 +0800

    docs(MinioSysFileService): 优化函数说明注释
    本次提交重点对MinioSysFileService类中的各个函数进行了注释的优化与补充，使得函数的功能、参数、返回值等信息更加清晰易懂。特别是upload和download函数，详细描述了函数内部的实现流程和注意事项。这将有助于其他开发者更好地理解和使用这些函数。

--- bcp-common/common-MinIO/src/main/java/cn/nfsn/common/minio/service/MinioSysFileService.java
+++ bcp-common/common-MinIO/src/main/java/cn/nfsn/common/minio/service/MinioSysFileService.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,9 +36,9 @@ public class MinioSysFileService {
     private String bucketName;
 
     /**
-     * description: 判断bucket是否存在，不存在则创建
+     * 判断bucket是否存在，不存在则创建
      *
-     * @return: void
+     * @param name 存储bucket名称
      */
     public void existBucket(String name) {
         try {
@@ -52,8 +53,9 @@ public class MinioSysFileService {
 
     /**
      * 创建存储bucket
+     *
      * @param bucketName 存储bucket名称
-     * @return Boolean
+     * @return Boolean 是否创建成功
      */
     public Boolean makeBucket(String bucketName) {
         try {
@@ -69,8 +71,9 @@ public class MinioSysFileService {
 
     /**
      * 删除存储bucket
+     *
      * @param bucketName 存储bucket名称
-     * @return Boolean
+     * @return Boolean   是否删除成功
      */
     public Boolean removeBucket(String bucketName) {
         try {
@@ -83,24 +86,35 @@ public class MinioSysFileService {
         }
         return true;
     }
+
     /**
-     * description: 上传文件
+     * 上传文件
      *
-     * @param file
-     * @return: java.lang.String
+     * @param file 待上传的文件对象
+     * @return 返回上传后的文件访问地址，如果上传失败则返回"Upload failed."
      */
     public String upload(MultipartFile file) {
+        // 获取原始文件名
         String fileName = file.getOriginalFilename();
+
+        // 将原始文件名分割成文件名和扩展名
         String[] split = fileName.split("\\.");
+
+        // 在原始文件名后添加当前时间戳以防止重名
         if (split.length > 1) {
+            // 如果文件有扩展名，则在文件名和扩展名之间添加时间戳
             fileName = split[0] + "_" + System.currentTimeMillis() + "." + split[1];
         } else {
+            // 如果文件没有扩展名，则直接在文件名后添加时间戳
             fileName = fileName + System.currentTimeMillis();
         }
 
         InputStream in = null;
         try {
+            // 获取文件输入流
             in = file.getInputStream();
+
+            // 使用Minio客户端将文件上传到指定的存储桶中
             minioClient.putObject(PutObjectArgs.builder()
                     .bucket(bucketName)
                     .object(fileName)
@@ -109,9 +123,11 @@ public class MinioSysFileService {
                     .build()
             );
         } catch (Exception e) {
+            // 如果在上传过程中发生异常，则打印堆栈轨迹并返回失败信息
             e.printStackTrace();
             return "Upload failed.";
         } finally {
+            // 关闭文件输入流
             if (in != null) {
                 try {
                     in.close();
@@ -120,40 +136,64 @@ public class MinioSysFileService {
                 }
             }
         }
+
+        // 返回上传后的文件访问地址
         return address + "/" + bucketName + "/" + fileName;
     }
 
-
     /**
-     * description: 下载文件
+     * 下载文件
      *
-     * @param fileName
-     * @return: org.springframework.http.ResponseEntity<byte [ ]>
+     * @param fileName 需要下载的文件名
+     * @return 返回包含文件内容的ResponseEntity对象，如果文件不存在或读取失败则返回null
      */
     public ResponseEntity<byte[]> download(String fileName) {
+        // 初始化响应实体对象
         ResponseEntity<byte[]> responseEntity = null;
+
         InputStream in = null;
         ByteArrayOutputStream out = null;
+
         try {
+            // 通过Minio客户端获取指定存储桶中的文件输入流
             in = minioClient.getObject(GetObjectArgs.builder().bucket(bucketName).object(fileName).build());
+
+            // 创建字节输出流
             out = new ByteArrayOutputStream();
+
+            // 将文件输入流的内容复制到字节输出流中
             IOUtils.copy(in, out);
-            //封装返回值
+
+            // 获取字节输出流中的数据
             byte[] bytes = out.toByteArray();
+
+            // 创建HTTP头信息
             HttpHeaders headers = new HttpHeaders();
             try {
+                // 添加内容处理方式为附件，并设置附件的文件名（进行URL编码防止特殊字符影响）
                 headers.add("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
             } catch (UnsupportedEncodingException e) {
+                // 如果在URL编码过程中发生异常，则打印堆栈轨迹
                 e.printStackTrace();
             }
+
+            // 设置内容长度为字节数组的长度
             headers.setContentLength(bytes.length);
+
+            // 设置内容类型为二进制流
             headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+            // 设置可公开访问的头部字段
             headers.setAccessControlExposeHeaders(Arrays.asList("*"));
+
+            // 封装响应实体，包括文件的字节数据、HTTP头信息和HTTP状态码
             responseEntity = new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
         } catch (Exception e) {
+            // 如果在获取文件或创建响应实体过程中发生异常，则打印堆栈轨迹
             e.printStackTrace();
         } finally {
             try {
+                // 关闭输入流和输出流
                 if (in != null) {
                     try {
                         in.close();
@@ -168,11 +208,14 @@ public class MinioSysFileService {
                 e.printStackTrace();
             }
         }
+
+        // 返回响应实体对象
         return responseEntity;
     }
 
     /**
      * 查看文件对象
+     *
      * @param bucketName 存储bucket名称
      * @return 存储bucket内文件对象信息
      */
@@ -197,8 +240,9 @@ public class MinioSysFileService {
 
     /**
      * 批量删除文件对象
+     *
      * @param bucketName 存储bucket名称
-     * @param objects 对象名称集合
+     * @param objects    对象名称集合
      */
     public Iterable<Result<DeleteError>> removeObjects(String bucketName, List<String> objects) {
         List<DeleteObject> dos = objects.stream().map(e -> new DeleteObject(e)).collect(Collectors.toList());
