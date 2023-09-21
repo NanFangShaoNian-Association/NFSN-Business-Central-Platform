7d6c9477a7f8a3e445863844cb9954142cdf2e11
Tuanzi <tuanziatclannad@qq.com>
2023-09-21 15:28:37 +0800

    build: 配置消息中心基础环境

--- bcp-modules/modules-chat/src/main/java/cn/nfsn/ChatPlatformApplication.java
+++ bcp-modules/modules-chat/src/main/java/cn/nfsn/ChatPlatformApplication.java
@@ -3,16 +3,18 @@ package cn.nfsn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @Author: gaojianjie
- * @Description TODO
+ * @Description 消息中心主启动程序
  * @date 2023/8/10 16:09
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOpenApi
 public class ChatPlatformApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ChatPlatformApplication.class,args);
+        SpringApplication.run(ChatPlatformApplication.class, args);
     }
 }
