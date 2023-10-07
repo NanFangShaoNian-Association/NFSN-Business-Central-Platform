188bd114308d495f6a5fdab48c231ff6f2155379
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-07 22:54:43 +0800

    refactor(IPayMode): 优化支付模式接口
    对IPayMode接口进行了部分方法的重命名和删除，以使其更符合实际需求和设计规则。具体修改如下：
    1. 删除了对订单处理和退款处理的方法：processOrder 和 processRefund。
    2. 将 handlePaymentNotification 方法更改为 paymentNotificationHandler。

--- bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/bridge/IPayMode.java
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/bridge/IPayMode.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Map;
 
 /**
  * @ClassName: IPayMode
@@ -19,31 +18,24 @@ import java.util.Map;
 public interface IPayMode {
 
    /**
-    * 创建订单的方法
+    * 创建订单
+    *
     * @param productDTO 商品数据传输对象,包含商品相关信息
     * @return           返回一个包含订单信息的Map对象
     * @throws Exception 如果在创建订单过程中出现问题，将抛出异常
     */
    Object createOrder(ProductDTO productDTO) throws Exception;
 
-   /**
-    * 处理支付通知，验证请求的有效性，并进行订单处理.
-    *
-    * @param request HttpServletRequest 对象，表示一个 HTTP 请求
-    * @return ResponsePayNotifyDTO    响应对象，包含响应码和信息
-    * @throws IOException               如果读取请求数据时出错
-    * @throws GeneralSecurityException  如果在验证签名过程中出现安全异常
-    */
-   ResponsePayNotifyDTO handlePaymentNotification(HttpServletRequest request, OrderStatus successStatus) throws IOException, GeneralSecurityException;
-
     /**
-    * 处理订单
-    *
-    * @param bodyMap                   请求体Map
-    * @throws GeneralSecurityException 抛出安全异常
-    */
-   @Transactional(rollbackFor = Exception.class)
-   void processOrder(Map<String, Object> bodyMap, OrderStatus successStatus) throws GeneralSecurityException;
+     * 处理来自支付系统方的支付通知
+     *
+     * @param request                    HttpServletRequest 对象，表示一个 HTTP 请求
+     * @param successStatus              订单状态
+     * @return ResponsePayNotifyDTO      响应对象，包含响应码和信息
+     * @throws IOException               如果读取请求数据时出错
+     * @throws GeneralSecurityException  如果在验证签名过程中出现安全异常
+     */
+   ResponsePayNotifyDTO paymentNotificationHandler(HttpServletRequest request, OrderStatus successStatus) throws IOException, GeneralSecurityException;
 
    /**
     * 取消订单
@@ -54,22 +46,12 @@ public interface IPayMode {
     void cancelOrder(String orderNo) throws Exception;
 
     /**
-     * 根据订单号和退款原因进行退款操作
+     * 退款
      *
-     * @param orderNo 订单编号，不能为空
-     * @param reason  退款原因，不能为空
+     * @param orderNo    订单编号，不能为空
+     * @param reason     退款原因，不能为空
      * @throws Exception 若退款过程中发生错误，则抛出异常
      */
     @Transactional(rollbackFor = Exception.class)
     void refund(String orderNo, String reason) throws Exception;
-
-    /**
-     * 处理退款单
-     *
-     * @param bodyMap 请求体Map，包含了退款信息
-     * @successStatus 成功状态
-     * @throws Exception 抛出异常，包括但不限于解密错误、数据库操作失败等
-     */
-    @Transactional(rollbackFor = Exception.class)
-    void processRefund(Map<String, Object> bodyMap, OrderStatus successStatus) throws Exception;
 }
