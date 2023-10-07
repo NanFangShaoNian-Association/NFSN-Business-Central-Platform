6efb6b065364375c04848301d8d450a048cfc4b9
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-07 22:53:20 +0800

    feat(RefundInfoService): 在退款信息服务中添加新的更新方法
    在RefundInfoService接口及其实现类RefundInfoServiceImpl中，新增了两个方法：updateRefund和updateRefundForAliPay。这两个方法用于根据提供的信息更新对应的退款记录。

--- bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/service/RefundInfoService.java
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/service/RefundInfoService.java
@@ -18,4 +18,20 @@ public interface RefundInfoService extends IService<RefundInfo> {
      * @return 返回创建的退款订单信息
      */
     RefundInfo createRefundByOrderNo(String orderNo, String reason);
+
+    /**
+     * 根据提供的JSON内容更新对应的退款记录
+     *
+     * @param content JSON格式的退款记录信息
+     */
+    void updateRefund(String content);
+
+    /**
+     * 更新退款记录
+     *
+     * @param refundNo     退款单编号
+     * @param content      响应结果内容
+     * @param refundStatus 退款状态
+     */
+    void updateRefundForAliPay(String refundNo, String content, String refundStatus);
 }
--- bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/service/impl/RefundInfoServiceImpl.java
+++ bcp-modules/modules-transaction/src/main/java/cn/nfsn/transaction/service/impl/RefundInfoServiceImpl.java
@@ -1,14 +1,24 @@
 package cn.nfsn.transaction.service.impl;
 
+import cn.nfsn.transaction.enums.WxRefundStatus;
 import cn.nfsn.transaction.model.entity.OrderInfo;
 import cn.nfsn.transaction.utils.OrderNoUtils;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import cn.nfsn.transaction.model.entity.RefundInfo;
 import cn.nfsn.transaction.service.RefundInfoService;
 import cn.nfsn.transaction.mapper.RefundInfoMapper;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static cn.nfsn.transaction.constant.AliPayConstant.REFUND_NO;
 
 /**
 * @author Atnibam Aitay
@@ -61,6 +71,87 @@ public class RefundInfoServiceImpl extends ServiceImpl<RefundInfoMapper, RefundI
 
         return refundInfo;
     }
+
+    /**
+     * 获取申请退款超过指定分钟数但还未成功的退款订单
+     *
+     * @param minutes 指定的分钟数
+     * @return 未成功的退款订单列表
+     */
+    public List<RefundInfo> getNoRefundOrderByDuration(int minutes) {
+        //计算出minutes分钟之前的时间点
+        Instant instant = Instant.now().minus(Duration.ofMinutes(minutes));
+
+        //构建查询条件
+        QueryWrapper<RefundInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("refund_status", WxRefundStatus.PROCESSING.getType());
+        queryWrapper.le("create_time", instant);
+
+        //查询并返回结果
+        return refundInfoMapper.selectList(queryWrapper);
+    }
+
+    /**
+     * 根据提供的JSON内容更新对应的退款记录
+     *
+     * @param content JSON格式的退款记录信息
+     */
+    @Override
+    public void updateRefund(String content) {
+        //将json字符串转换成Map
+        Gson gson = new Gson();
+        Map<String, String> resultMap = gson.fromJson(content, HashMap.class);
+        System.out.println(resultMap);
+
+        //构建查询条件
+        QueryWrapper<RefundInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("refund_no", resultMap.get("out_refund_no"));
+
+        //创建并初始化要更新的退款记录信息
+        RefundInfo refundInfo = new RefundInfo();
+        refundInfo.setRefundId(resultMap.get("refund_id"));
+
+        //查询退款和申请退款中的返回参数
+        if (resultMap.get("status") != null) {
+            refundInfo.setRefundStatus(resultMap.get("status"));
+            refundInfo.setContentReturn(content);
+        }
+        //退款回调中的回调参数
+        if (resultMap.get("refund_status") != null) {
+            refundInfo.setRefundStatus(resultMap.get("refund_status"));
+            refundInfo.setContentNotify(content);
+        }
+
+        //更新数据库中的退款记录
+        refundInfoMapper.update(refundInfo, queryWrapper);
+    }
+
+    /**
+     * 更新退款记录
+     *
+     * @param refundNo     退款单编号
+     * @param content      响应结果内容
+     * @param refundStatus 退款状态
+     */
+    @Override
+    public void updateRefundForAliPay(String refundNo, String content, String refundStatus) {
+
+        // 根据退款单编号构造查询条件
+        QueryWrapper<RefundInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq(REFUND_NO, refundNo);
+
+        // 构造要修改的退款信息对象，并设置退款状态与响应结果内容
+        RefundInfo refundInfo = new RefundInfo();
+        refundInfo.setRefundStatus(refundStatus);
+        refundInfo.setContentReturn(content);
+
+        // TODO: 还需要记录退款单ID
+
+
+        // 使用查询条件和待更新的退款信息，进行退款记录的更新操作
+        refundInfoMapper.update(refundInfo, queryWrapper);
+    }
+
 }
 
 
