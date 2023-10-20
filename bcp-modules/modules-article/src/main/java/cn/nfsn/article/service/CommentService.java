329fb49b9bdceb23b4ba6930b918d6487ee5c371
AtnibamAitay <atnibamaitay@foxmail.com>
2023-10-20 23:47:11 +0800

    feat(article): 新增评论树构建功能
    本次提交新增了一项功能，即在文章模块中添加了评论树的构建服务。定义了CommentService接口，并在CommentServiceImpl中实现了该接口。
    
    具体包含以下内容：
    1. 根据objectId、pageNum和pageSize构建评论树。
    2. 实现了根据objectId获取所有根评论的私有方法。
    3. 实现了根据父评论ID获取所有子评论的私有方法，如果子评论下还有子评论，也一并获取。
    4. 使用深度优先搜索遍历并构建评论树。
    
    此功能可支持应用在文章评论区展示完整的评论及其回复结构。

--- bcp-modules/modules-article/src/main/java/cn/nfsn/article/service/CommentService.java
+++ bcp-modules/modules-article/src/main/java/cn/nfsn/article/service/CommentService.java
@@ -1,13 +1,27 @@
 package cn.nfsn.article.service;
 
+import cn.nfsn.article.model.dto.CommentNodeDTO;
 import cn.nfsn.article.model.entity.Comment;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
-* @author Atnibam Aitay
-* @description 针对表【comment】的数据库操作Service
-* @createDate 2023-10-19 21:00:55
-*/
+ * @ClassName: CommentService
+ * @Description: 针对表【comment】的数据库操作Service
+ * @Author: AtnibamAitay
+ * @CreateTime: 2023-10-19 21:00
+ **/
 public interface CommentService extends IService<Comment> {
+
+    /**
+     * 根据objectId、pageNum和pageSize构建评论树
+     *
+     * @param objectId 对象ID
+     * @param pageNum  当前页码
+     * @param pageSize 每页大小
+     * @return 评论节点列表
+     */
+    List<CommentNodeDTO> buildCommentTree(Integer objectId, Integer pageNum, Integer pageSize);
 
 }
